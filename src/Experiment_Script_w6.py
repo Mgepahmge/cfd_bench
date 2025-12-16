c4f78ac619e6195ea37d60dacf7ed634ef147775
loki906 <2682011628@qq.com>
2025-12-16 16:24:49 +0800

    w6+w7

--- src/Experiment_Script_w6.py
+++ src/Experiment_Script_w6.py
@@ -1,70 +1,139 @@
-import Experiment_Framework_DBInterfaces as DB_API
+import DB_Interface_PG as DB_API
+import DB_Interface_IoTDB as IoTDB_API
 import numpy as np
 from numpy.typing import NDArray
-from vtk import vtkCell, vtkCellLocator, vtkCellTypes, vtkPlane, vtkPolyData, vtkIdTypeArray, vtkUnstructuredGrid, vtkIdList
+import VTK_Interface as VTK_API
+import os
+import random
+import time
 
 # A function that represents various simple tasks that should be implemented based on the context
-def place_holder():
+def calculate_force(normals: NDArray[np.float64], pressures: NDArray[np.float64]) -> NDArray[np.float64]:
+    total_force = np.array([0.0, 0.0, 0.0])
+    for i in range(len(normals)):
+        normal = normals[i]
+        pressure = pressures[i]
+        force = pressure * normal  # Assuming unit area for simplicity
+        total_force += force
+    return total_force
 
-    return None
+def main():
+
+    ''' Pre-processing '''
+
+    # pg_entity = DB_API.PG_Interface.pg_connect()
+    pg_api = DB_API.PG_Interface()
+    pg_entity = pg_api.pg_connect()
+
+    iotdb_api = IoTDB_API.Iotdb_Interface()
+    iotdb_entity = iotdb_api.iotdb_connect()
+
+    # tiledb_entity = DB_API.Tiledb_Interface.tiledb_connect()
+
+    vtk_api = VTK_API.VTK_Interface()
 
 
-''' Pre-processing '''
+    ''' Define ship types and time_step for query'''
+    SHIP_TYPE_LIST = ["JBC_615k", "JBC_3843k", "Kvlcc2_351k", "Kvlcc2_3709k", "Suboff_3258k"]
+    TIME_STEP_LIST = ["200", "400", "600", "800", "1000", "1200", "1400", "1600", "1800", "2000"]
 
-# Notice, in w6, we are loading the hull_zone data instead of fluid_zone data
+    VTK_QUERY_DIR = "../vtk_db_dir/"
+    VTK_HULL_DIR = "../vtk_hull_dir/"
 
-pg_entity = DB_API.PG_Interface.pg_connect()
+    vtk_query_files = [f for f in os.listdir(VTK_QUERY_DIR) if f.endswith(".vtk")]
+    vtk_hull_files = [f for f in os.listdir(VTK_HULL_DIR) if f.endswith(".vtk")]
 
-iotdb_entity = DB_API.Iotdb_Interface.iotdb_connect()
 
-tiledb_entity = DB_API.Tiledb_Interface.tiledb_connect()
+    ''' Workload 6 '''
+    for ship_type in SHIP_TYPE_LIST:
 
-vtk_entity = DB_API.VTK_Interface.vtk_connect() # Load timestep vtk files
+        for time_step in TIME_STEP_LIST:
 
-vtk_mesh = DB_API.VTK_Interface.vtk_connect # Load the geo structure of the ship
+            # 跳过不存在的 2000 时间步
+            if time_step == "2000" and (ship_type == "JBC_3843k" or ship_type == "Kvlcc2_3709k"):
+                # print(f"跳过 {ship_type} 的时间步 2000，该数据集无此时间步")
+                continue  # 跳过当前时间步，继续下一个
 
-''' Workload 6 '''
+            # vtk_hull_file, = [f for f in vtk_hull_files if ship_type in f]
+            vtk_hull_file, = [f for f in vtk_hull_files if (ship_type in f) and f.endswith(f"_{time_step}.vtk")]
+            vtk_hull_mesh = VTK_API.VTK_Interface().vtk_connect(os.path.join(VTK_HULL_DIR, vtk_hull_file))
 
-hull_zone_length = -1 # TODO: compute the number of elements of hull zone
+            ''' W 6.1  Testing pg ... '''
+            if "_" in ship_type:
+                parts = ship_type.split("_")
+                pg_api.set_ship_type(parts[0])  # 设置船型
+                pg_api.set_ship_scale("_".join(parts[1:]))  # 设置规模
+            pg_api.set_zone_type("hull")
 
-all_indexes = np.arange(0, hull_zone_length)
+            pg_api.set_time_step(time_step)
 
-''' W 6.1  Testing pg ... '''
+            print("\nTesting PostgreSQL workload6 for ship type:", ship_type, " at time step:", time_step)
 
-norm_vectors = DB_API.VTK_Interface.vtk_surface_norm(vtk_mesh) # load the hull_zone and compute the norms of each element
+            pg_transaction = 0
+            start_time = time.time()
 
-pressures = DB_API.PG_Interface.point_query(pg_entity, all_indexes, ['P']) # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
+            while time.time() - start_time < 60:
+                
+                pg_norm_vectors = VTK_API.VTK_Interface.vtk_surface_norm(vtk_hull_mesh) # load the hull_zone and compute the norms of each element
 
-aggregated_force = norm_vectors * pressures
+                pressures = pg_api.point_query(pg_entity, np.array(range(vtk_hull_mesh.GetNumberOfCells())), 'P') # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
 
-# print(aggregated_force[0])
+                aggregated_force = calculate_force(pg_norm_vectors, pressures)
 
-''' W 6.2  Testing iotdb ... '''
+                pg_transaction += 1
 
-norm_vectors = DB_API.VTK_Interface.vtk_surface_norm(vtk_mesh) # load the hull_zone and compute the norms of each element
+            print("PostgreSQL workload6 transactions for ship type:", ship_type, " at time step:", time_step, " is ", pg_transaction)
 
-pressures = DB_API.Iotdb_Interface.point_query(iotdb_entity, all_indexes, ['P']) # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
+            ''' W 6.2  Testing iotdb ... '''
+            iotdb_api.set_ship_type(ship_type + "hull")
+            iotdb_api.set_time_step(time_step)
+            print("\nTesting IoTDB workload6 for ship type:", ship_type, " at time step:", time_step)
+            iotdb_transcation = 0
+            
+            start_time = time.time()
+            while time.time() - start_time < 60:
 
-aggregated_force = norm_vectors * pressures
+                iotdb_norm_vectors = VTK_API.VTK_Interface.vtk_surface_norm(vtk_hull_mesh) # load the hull_zone and compute the norms of each element
 
-# print(aggregated_force[0])
+                pressures = iotdb_api.point_query(iotdb_entity, np.array(range(vtk_hull_mesh.GetNumberOfCells())), 'P') # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
 
-''' W 6.3  Testing tiledb ... '''
+                aggregated_force = calculate_force(iotdb_norm_vectors, pressures)
 
-norm_vectors = DB_API.VTK_Interface.vtk_surface_norm(vtk_mesh) # load the hull_zone and compute the norms of each element
+                iotdb_transcation += 1
 
-pressures = DB_API.Tiledb_Interface.point_query(tiledb_entity, all_indexes, ['P']) # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
+            print("IoTDB workload6 transactions for ship type:", ship_type, " at time step:", time_step, " is ", iotdb_transcation)
 
-aggregated_force = norm_vectors * pressures
+            ''' W 6.3  Testing tiledb ... '''
 
-# print(aggregated_force[0])
+            # norm_vectors = DB_API.VTK_Interface.vtk_surface_norm(vtk_mesh) # load the hull_zone and compute the norms of each element
 
-''' W 6.4  Testing vtk as db ... '''
+            # pressures = DB_API.Tiledb_Interface.point_query(tiledb_entity, all_indexes, ['P']) # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
 
-norm_vectors = DB_API.VTK_Interface.vtk_surface_norm(vtk_mesh) # load the hull_zone and compute the norms of each element
+            # aggregated_force = norm_vectors * pressures
 
-pressures = DB_API.VTK_Interface.point_query(vtk_entity, all_indexes, ['P']) # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
+            # print(aggregated_force[0])
 
-aggregated_force = norm_vectors * pressures
+            ''' W 6.4  Testing vtk as db ... '''
+            
+            print("\nTesting VTK workload6 for ship type:", ship_type, " at time step:", time_step)
 
-# print(aggregated_force[0])
+            vtk_transaction = 0
+            start_time = time.time()
+
+            while time.time() - start_time < 60:
+                # vtk_entity = vtk_api.vtk_connect(os.path.join(VTK_HULL_DIR, vtk_hull_file))
+
+                vtk_norm_vectors = VTK_API.VTK_Interface.vtk_surface_norm(vtk_hull_mesh) # load the hull_zone and compute the norms of each element
+
+                pressures = vtk_api.point_query(vtk_hull_mesh, np.array(range(vtk_hull_mesh.GetNumberOfCells())), 'P') # The purpose is to retrieve all pressure values, one can choose to implement in the most efficient way.
+
+                aggregated_force = calculate_force(vtk_norm_vectors, pressures)
+
+                vtk_transaction += 1
+
+            print("VTK workload6 transactions for ship type:", ship_type, " at time step:", time_step, " is ", vtk_transaction)
+            
+            # 需要每个ship每个timestep vtk_hull 5*10 files
+
+if __name__ == "__main__":
+    main()
--- src/Experiment_Script_w7.py
+++ src/Experiment_Script_w7.py
@@ -1,60 +1,161 @@
-import Experiment_Framework_DBInterfaces as DB_API
+import DB_Interface_PG as DB_API
+import DB_Interface_IoTDB as IoTDB_API
 import numpy as np
 from numpy.typing import NDArray
-from vtk import vtkCell, vtkCellLocator, vtkCellTypes, vtkPlane, vtkPolyData, vtkIdTypeArray, vtkUnstructuredGrid, vtkIdList
+from vtk import vtkUnstructuredGrid ,vtkGradientFilter, vtkDataObject
+import VTK_Interface as VTK_API
+import os
+import random
+import time
 
-# A function that represents various simple tasks that should be implemented based on the context
-def place_holder():
+def random_range(vtk_mesh:vtkUnstructuredGrid) -> NDArray[np.float64]:
+    bounds = vtk_mesh.GetBounds()
+    x1, x2 = sorted([random.uniform(bounds[0], bounds[1]), random.uniform(bounds[0], bounds[1])])
+    y1, y2 = sorted([random.uniform(bounds[2], bounds[3]), random.uniform(bounds[2], bounds[3])])
+    z1, z2 = sorted([random.uniform(bounds[4], bounds[5]), random.uniform(bounds[4], bounds[5])])
+    return np.array([[x1, y1, z1], [x2, y2, z2]])
 
-    return None
+def ComputeQCriterion(vtk_mesh:vtkUnstructuredGrid) -> NDArray[np.float64]:
+
+    #for this function, we assume that the velocity vector is already constructed in the vtk_mesh
+
+    velocity_array = vtk_mesh.GetPointData().GetArray("Velocity")
+    if velocity_array:
+        gradient_filter = vtkGradientFilter()
+        gradient_filter.SetInputData(vtk_mesh)
+        gradient_filter.SetInputArrayToProcess(0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_POINTS, "Velocity")
+        gradient_filter.SetComputeQCriterion(True)
+        gradient_filter.Update()
+        mesh_with_q = gradient_filter.GetOutput()
+        QCriterion_array = mesh_with_q.GetPointData().GetArray("QCriterion")
+        return QCriterion_array
+        
+def main():
+
+    ''' Pre-processing '''
+
+    # pg_entity = DB_API.PG_Interface.pg_connect()
+    pg_api = DB_API.PG_Interface()
+    pg_entity = pg_api.pg_connect()
+
+    iotdb_api = IoTDB_API.Iotdb_Interface()
+    iotdb_entity = iotdb_api.iotdb_connect()
+
+    # tiledb_entity = DB_API.Tiledb_Interface.tiledb_connect()
+
+    vtk_api = VTK_API.VTK_Interface()
 
 
-''' Pre-processing '''
+    ''' Define ship types and time_step for query'''
+    SHIP_TYPE_LIST = ["JBC_615k", "JBC_3843k", "Kvlcc2_351k", "Kvlcc2_3709k", "Suboff_3258k"]
+    TIME_STEP_LIST = ["200", "400", "600", "800", "1000", "1200", "1400", "1600", "1800", "2000"]
+    VTK_MESH_DIR = "../vtk_dir/"
+    # VTK_QUERY_DIR = "../vtk_db_dir/"
+   
+    vtk_geo_files = [f for f in os.listdir(VTK_MESH_DIR) if f.endswith(".vtk")]
+    # vtk_query_files = [f for f in os.listdir(VTK_QUERY_DIR) if f.endswith(".vtk")]
+    
 
-pg_entity = DB_API.PG_Interface.pg_connect()
 
-iotdb_entity = DB_API.Iotdb_Interface.iotdb_connect()
+    ''' Workload 7 '''
+    for ship_type in SHIP_TYPE_LIST:
 
-tiledb_entity = DB_API.Tiledb_Interface.tiledb_connect()
+        # vtk_file, = [f for f in vtk_geo_files if ship_type in f]
+        # vtk_mesh = VTK_API.VTK_Interface().vtk_connect(os.path.join(VTK_MESH_DIR, vtk_file))
 
-vtk_entity = DB_API.VTK_Interface.vtk_connect() # Load timestep vtk files
+        for time_step in TIME_STEP_LIST:
 
-vtk_mesh = DB_API.VTK_Interface.vtk_connect # Load the geo structure of the ship
+            # 跳过不存在的 2000 时间步
+            if time_step == "2000" and (ship_type == "JBC_3843k" or ship_type == "Kvlcc2_3709k"):
+                # print(f"跳过 {ship_type} 的时间步 2000，该数据集无此时间步")
+                continue  # 跳过当前时间步，继续下一个
 
-''' Workload 7 '''
+            vtk_file, = [f for f in vtk_geo_files if (ship_type in f) and f.endswith(f"_{time_step}.vtk")]
+            vtk_mesh = VTK_API.VTK_Interface().vtk_connect(os.path.join(VTK_MESH_DIR, vtk_file))
 
-ranges: NDArray[np.float64] = place_holder() # TODO: Generateing a range in the form of [[x_min, x_max], [y_min, y_max], [z_min, z_max]]
+            ''' W 7.1  Testing pg ... '''
+            if "_" in ship_type:
+                parts = ship_type.split("_")
+                pg_api.set_ship_type(parts[0])  # 设置船型
+                pg_api.set_ship_scale("_".join(parts[1:]))  # 设置规模
+            pg_api.set_zone_type("fluid")
 
-COORD = ['X','Y','Z'] # Its just a macro
+            pg_api.set_time_step(time_step)
 
-''' W 7.1  Testing pg ... '''
+            print("\nTesting PostgreSQL workload7 for ship type:", ship_type, " at time step:", time_step)
+            pg_transaction = 0
+            start_time = time.time()
+            while time.time() - start_time < 60:
+                while True:
+                    lower_bound, upper_bound = random_range(vtk_mesh)
+                    cell_indexes = pg_api.range_query_coord(pg_entity, lower_bound, upper_bound)
+                    if cell_indexes.size > 0:
+                        break
+                
+                sub_mesh = VTK_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_mesh)
 
-cell_indexes = DB_API.PG_Interface.range_query(pg_entity, ranges, COORD)
+                pg_QCriterion = ComputeQCriterion(sub_mesh)
 
-sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
+                pg_transaction += 1
 
-DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
+            print("PostgreSQL workload7 transactions for ship type:", ship_type, " at time step:", time_step, " is ", pg_transaction)
 
-''' W 7.2  Testing iotdb ... '''
+            ''' W 7.2  Testing iotdb ... '''
 
-cell_indexes = DB_API.Iotdb_Interface.range_query(iotdb_entity, ranges, COORD)
+            iotdb_api.set_ship_type(ship_type)
+            iotdb_api.set_time_step(time_step)
+            print("\nTesting IoTDB workload7 for ship type:", ship_type, " at time step:", time_step)
+            iotdb_transaction = 0
+            start_time = time.time()
+            while time.time() - start_time < 60:
+                while True:
+                    lower_bound, upper_bound = random_range(vtk_mesh)
+                    cell_indexes = iotdb_api.range_query_coord(iotdb_entity, lower_bound, upper_bound)
+                    if cell_indexes.size > 0:
+                        break
+                
+                sub_mesh = VTK_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_mesh)
 
-sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
+                iotdb_QCriterion = ComputeQCriterion(sub_mesh)
 
-DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
+                iotdb_transaction += 1
 
-''' W 7.3  Testing tiledb ... '''
+            print("IoTDB workload7 transactions for ship type:", ship_type, " at time step:", time_step, " is ", iotdb_transaction)
 
-cell_indexes = DB_API.Tiledb_Interface.range_query(tiledb_entity, ranges, COORD)
 
-sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
+            ''' W 7.3  Testing tiledb ... '''
 
-DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
+            # cell_indexes = DB_API.Tiledb_Interface.range_query(tiledb_entity, ranges, COORD)
 
-''' W 7.4  Testing vtk as db ... '''
+            # sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
 
-cell_indexes = DB_API.VTK_Interface.range_query(vtk_entity, ranges, COORD)
+            # DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
 
-sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
+            ''' W 7.4  Testing vtk as db ... '''
+            print("\nTesting VTK workload7 for ship type:", ship_type, " at time step:", time_step)
+            vtk_transaction = 0
+            start_time = time.time()
+            while time.time() - start_time < 60:
+                while True:
+                    lower_bound, upper_bound = random_range(vtk_mesh)
+                    cell_indexes = vtk_api.range_query_coord(vtk_mesh, lower_bound, upper_bound)
+                    if cell_indexes.size > 0:
+                        break
+                
+                sub_mesh = VTK_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_mesh)
 
-DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
+                vtk_QCriterion = ComputeQCriterion(sub_mesh)
+
+                vtk_transaction += 1
+
+            print("VTK workload7 transactions for ship type:", ship_type, " at time step:", time_step, " is ", vtk_transaction)
+
+            # cell_indexes = DB_API.VTK_Interface.range_query(vtk_entity, ranges, COORD)
+
+            # sub_mesh = DB_API.VTK_Interface.vtk_extract_submesh(cell_indexes, vtk_entity)
+
+            # DB_API.VTK_Interface.vtk_Q_criterion(sub_mesh)
+
+
+if __name__ == "__main__":
+    main()
